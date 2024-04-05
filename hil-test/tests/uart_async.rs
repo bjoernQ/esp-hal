d0b0cc7ddf6f5de976b9feed47c6472759f54062
bjoernQ <bjoern.quentin@mobile-j.de>
2024-04-05 09:26:35 +0200

    Experimenting

--- hil-test/tests/uart_async.rs
+++ hil-test/tests/uart_async.rs
@@ -8,27 +8,25 @@
 
 #![no_std]
 #![no_main]
+#![feature(type_alias_impl_trait)]
 
 use defmt_rtt as _;
 use esp_backtrace as _;
 use esp_hal::{
-    clock::ClockControl,
-    gpio::IO,
-    peripherals::{Peripherals, UART0},
-    prelude::*,
-    uart::{
+    clock::ClockControl, gpio::IO, peripherals::{Peripherals, TIMG0, UART0}, prelude::*, timer::TimerGroup, uart::{
         config::{Config, DataBits, Parity, StopBits},
         TxRxPins,
         Uart,
         UartRx,
         UartTx,
-    },
-    Async,
+    }, Async
 };
 
 struct Context {
     tx: UartTx<'static, UART0, Async>,
     rx: UartRx<'static, UART0, Async>,
+    clocks: esp_hal::clock::Clocks<'static>,
+    timer: esp_hal::timer::TimerGroup<'static, TIMG0, esp_hal::Async>,
 }
 
 impl Context {
@@ -51,36 +49,58 @@ impl Context {
         let uart = Uart::new_async_with_config(peripherals.UART0, config, Some(pins), &clocks);
         let (tx, rx) = uart.split();
 
-        Context { rx, tx }
+        let timer = TimerGroup::new_async(peripherals.TIMG0, &clocks);
+        Context { rx, tx, clocks, timer }
     }
 }
+
+
+#[embassy_executor::task]
+async fn test_send_receive(
+    mut tx: UartTx<'static, UART0, Async>,
+    mut rx: UartRx<'static, UART0, Async>,
+) {
+    const SEND: &[u8] = &*b"Hello ESP32";
+    let mut buf = [0u8; SEND.len()];
+
+    defmt::info!("here");
+    // Drain the FIFO to clear previous message:
+    tx.flush_async().await.unwrap();
+    while rx.drain_fifo(&mut buf[..]) > 0 {}
+
+    tx.write_async(&SEND).await.unwrap();
+    tx.flush_async().await.unwrap();
+
+    rx.read_async(&mut buf[..]).await.unwrap();
+    assert_eq!(&buf[..], SEND);
+
+    embedded_test::export::check_outcome(());
+}
+
+
 
 #[cfg(test)]
 #[embedded_test::tests]
 mod tests {
     use defmt::assert_eq;
+    use static_cell::make_static;
 
     use super::*;
 
     #[init]
-    async fn init() -> Context {
+    fn init() -> Context {
         Context::init()
     }
 
     #[test]
     #[timeout(3)]
-    async fn test_send_receive(mut ctx: Context) {
-        const SEND: &[u8] = &*b"Hello ESP32";
-        let mut buf = [0u8; SEND.len()];
+    fn test_send_receive(mut ctx: Context) {
+        esp_hal::embassy::init(&ctx.clocks, ctx.timer);
 
-        // Drain the FIFO to clear previous message:
-        ctx.tx.flush_async().await.unwrap();
-        while ctx.rx.drain_fifo(&mut buf[..]) > 0 {}
-
-        ctx.tx.write_async(&SEND).await.unwrap();
-        ctx.tx.flush_async().await.unwrap();
-
-        ctx.rx.read_async(&mut buf[..]).await.unwrap();
-        assert_eq!(&buf[..], SEND);
+        let executor = esp_hal::embassy::executor::thread::Executor::new();
+        let executor = make_static!(executor);
+        executor.run(|spawner|{
+            spawner.spawn(super::test_send_receive(ctx.tx, ctx.rx)).ok();
+        });
     }
 }
